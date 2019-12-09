e2cb9292e79e0756aa8409f83f653e84dc7728ba
cloudacy server <server@cloudacy.io>
2019-12-09 22:28:10 +0000

    Remove release shell file.

--- release.sh
+++
@@ -1,9 +0,0 @@
-yarn run build;
-echo "Following files and directories will be changed:";
-rsync -arzhni --delete ./dist/ averi@averi.at:/var/www/vhosts/averi.at/httpdocs/schnellen;
-echo "";
-read -p "Do you want to release? [y/N]" choice
-case "$choice" in
-    y|Y ) rsync -arhz --delete ./dist/ averi@averi.at:/var/www/vhosts/averi.at/httpdocs/schnellen;;
-    * ) echo "Release aborted.";;
-esac
