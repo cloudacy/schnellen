1ef4eeef054ff99c9ac7608ffcda009bdd09ae6b
d-kuen <d.kuen@cloudacy.com>
2025-08-12 15:42:26 +0200

    fix: add missing Label component

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -9,6 +9,7 @@ import {useGameStore} from '@/stores/game'
 import {Button} from '@/components/ui/button'
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from '@/components/ui/card'
 import {Input} from '@/components/ui/input'
+import {Label} from '@/components/ui/label'
 
 const game = useGameStore()
 
